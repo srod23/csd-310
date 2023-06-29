4c5545a2186a2a7db4bd49f4be441b2f0e602017
srod23 <133072880+srod23@users.noreply.github.com>
2023-06-29 15:01:41 -0400

    Update pytech_queries.py

--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -1,7 +1,18 @@
 from mongodb_test import db
 
-harry_doc = db.students.find_one({"student_id": "1007"})
-ron_doc = db.students.find_one({"student_id": "1008"})
-hermoine_doc = db.students.find_one({"student_id": "1009"})
 
-print(harry_doc, ron_doc, hermoine_doc)
+# FIND({})
+docs = db.students.find({})
+print('Find All: ')
+for doc in docs:
+  print(doc)
+
+#FIND.ONE({})
+harry_doc = db.students.find_one({"student_id": "1007"})
+print('Find One: ', harry_doc)
+
+ron_doc = db.students.find_one({"student_id": "1008"})
+# print(ron_doc)
+
+hermoine_doc = db.students.find_one({"student_id": "1009"})
+# print(hermoine_doc)
